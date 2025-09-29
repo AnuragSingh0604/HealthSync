432ac10b4869edb97ada5ce60918793fc9362fbc
Anurag singh <anurag2307singh@gmail.com>
2025-09-29 17:59:54 +0530

    updated home section

--- Frontend/src/components/TopDoctors.jsx
+++ Frontend/src/components/TopDoctors.jsx
@@ -1,4 +1,4 @@
-import React, { use, useContext } from 'react'
+import React, { useContext } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { AppContext } from '../contexts/AppContext'
 
@@ -7,7 +7,7 @@ const TopDoctors = () => {
     const navigate=useNavigate();
     const {doctors}=useContext(AppContext);
   return (
-    <div className='flex flex-col items-center gap-4 my-16 tetx-gray-900 md:mx-10'>
+    <div className='flex flex-col items-center gap-4 my-16 text-gray-900 md:mx-10'>
         <h1 className='text-3xl font-medium'>Top Doctors to Book</h1>
         <p className='sm:w-1/3 text-center text-sm '>Simply browse through our extensive list of trusted doctors</p>
         <div className='w-full grid grid-cols-auto gap-4 pt-5 gap-y-6 px-3 sm:px-0'>
@@ -24,7 +24,7 @@ const TopDoctors = () => {
                 </div>
             ))}
         </div>
-        <button onClick={()=>{navigate('/doctors'); scrollTo(0,0)}}className='bg-blue-50 text-gray-600 px-12 py-3 rounded-full mt-10'>more</button>
+        <button onClick={()=>{navigate('/doctors'); scrollTo(0,0)}} className='bg-blue-50 text-gray-600 px-12 py-3 rounded-full mt-10'>more</button>
     </div>
   )
 }
