adcb66468894340d8d35772881e1d56faac47b40
Anurag singh <anurag2307singh@gmail.com>
2025-10-20 19:44:20 +0530

    added add doctor api

---
+++ backend/controllers/adminController.js
@@ -0,0 +1,46 @@
+import validator from 'validator';
+import bcrypt from 'bcrypt';
+import Doctor from '../models/doctorModel.js';
+import {v2 as cloudinary} from 'cloudinary';
+
+const addDoctor=async(req,res)=>{
+    try{
+        const {name,email,password,speciality,experience,degree,about,fees,address,}=req.body;
+        const image=req.file;
+        if(!name || !email || !password || !speciality || !experience || !degree || !about || !fees || !address ){
+        
+            return res.status(400).json({success:false,message:"all fields are required"})}
+        if(!validator.isEmail(email)){    
+            return res.status(400).json({success:false,message:"invalid email"});}
+        if(password.length<8){
+            return res.status(400).json({success:false,message:"password must be at least 8 characters long"});}
+            const salt=await bcrypt.genSalt(10);
+            const hashedPassword=await bcrypt.hash(password,salt);
+            
+            const uploadedImage=await cloudinary.uploader.upload(image.path,{resource_type  :'image'});
+            const imageUrl=uploadedImage.secure_url;
+            const newDoctor=new Doctor({
+                name,
+                email,
+                password:hashedPassword,
+                speciality,
+                experience,
+                degree,
+                about,
+                availability:true,
+                fees,
+                address:JSON.parse(address),
+                date:Date.now(),
+                image:imageUrl
+            });
+            await newDoctor.save();
+            res.status(201).json({success:true,message:"doctor added successfully"});
+    }
+    catch(error){
+        console.error("Error in addDoctor:",error);
+        res.status(500).json({success:false,message:error.message});
+        
+        }}
+
+
+    export {addDoctor};
